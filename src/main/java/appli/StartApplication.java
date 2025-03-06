98b48c3364f26c9e4432bb0867ed2669b60836bb
Elias ZOHRI CHAKKOUF <ZOHRI-CHAKKOUF_El@lprs.local>
2025-03-06 17:25:46 +0100

    debut de la page inscrition

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -9,12 +9,19 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("acceuil/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Bienvenue!");
         stage.setScene(scene);
         stage.show();
     }
+    public static void changeScene(String nomDuFichierFxml ) throws IOException {
+        FXMLLoader fxmlLoader = new
+                FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml + "acceuil/InscriptionView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        mainStage.setScene(scene);
+    }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/appli/acceuil/LoginController.java
+++ src/main/java/appli/acceuil/LoginController.java
@@ -1,28 +1,37 @@
 package appli.acceuil;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    private Label text2;
+    @FXML
+    private TextField email;
     @FXML
     private PasswordField mdp;
-    @FXML
-    private Label text1;
     @FXML
     private Label welcomeText;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-    protected void setText1(String text) {
-        text1.setText(text);
-    }
     protected void setMdp(PasswordField mdp) {
         this.mdp = mdp;
     }
-    protected void text2(String text) {
-        text2.setText(text);
+    protected void setEmail(TextField email) {
+        this.email = email;
+    }
+    @FXML
+    protected void bouttonConnexion(ActionEvent actionEvent){
+        this.email.setText("test@gmail.com");
+        this.mdp.setText("123");
+    }
+
+    @FXML
+    protected void buttonMDP(ActionEvent actionEvent) {
+    this.setMdp(mdp);
     }
 }
